d49f45e23861f6dba8c6620ae39632faaf1106c6
Joon Kang <tnjnkng@gmail.com>
2021-03-23 12:41:47 +0800

    fixed navbar bug

--- core/static/assets/js/black-dashboard.js
+++ core/static/assets/js/black-dashboard.js
@@ -102,14 +102,14 @@ $(document).ready(function() {
     }
   });
 
-  if ($('.full-screen-map').length == 0 && $('.bd-docs').length == 0) {
-    // On click navbar-collapse the menu will be white not transparent
-    $('.collapse').on('show.bs.collapse', function() {
-      $(this).closest('.navbar').removeClass('navbar-transparent').addClass('bg-white');
-    }).on('hide.bs.collapse', function() {
-      $(this).closest('.navbar').addClass('navbar-transparent').removeClass('bg-white');
-    });
-  }
+  // if ($('.full-screen-map').length == 0 && $('.bd-docs').length == 0) {
+  //   // On click navbar-collapse the menu will be white not transparent
+  //   $('.collapse').on('show.bs.collapse', function() {
+  //     $(this).closest('.navbar').removeClass('navbar-transparent').addClass('bg-white');
+  //   }).on('hide.bs.collapse', function() {
+  //     $(this).closest('.navbar').addClass('navbar-transparent').removeClass('bg-white');
+  //   });
+  // }
 
   blackDashboard.initMinimizeSidebar();
 
